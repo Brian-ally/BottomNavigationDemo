e3f93b24d0e225a1e105b35fc6de711b94af5d6b
Liangbaolin <1983531396@qq.com>
2020-06-05 19:13:57 +0800

    MainActivity.java:装配底部导航栏和碎片容器

--- app/src/main/java/com/example/bottomnavigationdemo/MainActivity.java
+++ app/src/main/java/com/example/bottomnavigationdemo/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.bottomnavigationdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 /**
  * @author 19835
@@ -13,5 +19,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //初始化底部导航栏和碎片容器
+        BottomNavigationView bottomnavigationview = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this,R.id.fragment);
+        //装配，形成关联
+        AppBarConfiguration configuration = new AppBarConfiguration.Builder(bottomnavigationview.getMenu()).build();
+        NavigationUI.setupActionBarWithNavController(this,navController,configuration);
+        NavigationUI.setupWithNavController(bottomnavigationview,navController);
+
     }
 }
